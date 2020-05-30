e0e3f1b7999a8ea8ba7dfece8745cc6b6b2bf728
DiamondYuan <admin@diamondyuan.com>
2020-05-30 23:36:53 +0800

    :bug: fix load script

--- load.js
+++ load.js
@@ -3,7 +3,11 @@ const { execSync } = require("child_process");
 const { writeFileSync } = require("fs");
 const start = path.join(__dirname, "script", "start.sh");
 const index = path.join(__dirname, "index.js");
-const result = execSync("which node").toString().split("\n")[0];
+const result = execSync("which node", {
+  shell: true,
+})
+  .toString()
+  .split("\n")[0];
 
 const content = `${result} ${index}`;
 
